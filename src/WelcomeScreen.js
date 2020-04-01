cf06d28c7c3ee8b3870a86a44c5bc39ddadf1adc
DjOlaiya <djolaiya@gmail.com>
2020-04-01 00:03:53 -0700

    included signout btn

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-// import MuiThemeProvider from 'material-ui/core/styles/MuiThemeProvider';
+import Button from '@material-ui/core/Button';
 import Login from './Login';
 import Register from './Register';
-
+import { Auth } from "aws-amplify";
 class WelcomeScreen extends Component 
 {
+
+    signout = async() => {
+        try {
+            Auth.signOut();
+            console.log("Success Signout")
+          } catch (err) {
+            console.log(err);
+          }
+    }
     render() {
         return (
           <div className="welcomescreen">
               <h1> Congrats on logging in</h1>
+              <Button 
+                            variant="contained"
+                             style={style}
+                             onClick = {this.signout}>
+                           Sign Out  
+                            </Button> 
           </div>
         );
     }
